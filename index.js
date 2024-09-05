f159b3c157c29bf1717049ef5503501b4fe68d01
Dmytro Iovenko <iovenko@gmail.com>
2024-09-05 14:58:31 -0400

    add Logging Middleware

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 
+// Logging Middleware
+app.use((req, res, next) => {
+  const time = new Date().toLocaleTimeString();
+  console.log(`-----
+    ${time}: Received a ${req.method} request to ${req.url}.`);
+  if (Object.keys(req.body.length > 0)) {
+    console.log("Containing the data:");
+    console.log(`${JSON.stringify(req.body)}`);
+  }
+  next();
+});
+
 // Start express server
 app.listen(3000, () => {
   console.log("Server is running on port:", port);
