a7d5219d40fd25e0198d8a8b9a0f8a019e6ec736
Dmytro Iovenko <iovenko@gmail.com>
2024-09-05 14:40:35 -0400

    add Parsing Body middleware

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-// Start express server 
+// Parsing Body Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+
+// Start express server
 app.listen(3000, () => {
   console.log("Server is running on port:", port);
 });
