7b6374487b81e2fec4ebf371463595b40b8e6f55
Dmytro Iovenko <iovenko@gmail.com>
2024-09-05 15:44:52 -0400

    improve logging and error-handling

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-const error = require("./utils/error")
+const error = require("./utils/error");
 
 // Parsing Body Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,10 +11,10 @@ app.use(bodyParser.json({ extended: true }));
 
 // Logging Middleware
 app.use((req, res, next) => {
-  const time = new Date().toLocaleTimeString();
-  console.log(`-----
-    ${time}: Received a ${req.method} request to ${req.url}.`);
-  if (Object.keys(req.body.length > 0)) {
+  const time = new Date().toLocaleString();
+  console.log("-----");
+  console.log(`${time}: Received a ${req.method} request to ${req.url}.`);
+  if (Object.keys(req.body).length > 0) {
     console.log("Containing the data:");
     console.log(`${JSON.stringify(req.body)}`);
   }
@@ -26,14 +26,20 @@ app.use((req, res, next) => {
   throw error(404, "Resource Not Found");
 });
 
-
 // Error-handling Middleware
 // Any call to next() that includes an Error() will skip regular middleware
 // and only be processed by error-handling middleware.
 app.use((err, req, res, next) => {
+  const time = new Date();
   res.status(err.status || 500);
-  res.json({ error: err.message });
-  console.error(err);
+  res.json({
+    status: err.status,
+    error: err.message,
+    timestamp: time.toISOString(),
+    path: req.url,
+  });
+  // Log non-standard (server) errors
+  !err.status && console.error(`${time.toLocaleString()}: ${err.stack}`);
 });
 
 // Start express server
