42857d5b291ee3fe6532ba4048a6de191cdcabd0
Dmytro Iovenko <iovenko@gmail.com>
2024-09-06 03:36:30 -0400

    add Validating New Course Middleware

--- routes/courses.js
+++ routes/courses.js
@@ -5,6 +5,16 @@ const error = require("../utils/error");
 // Get data
 const courses = require("../data/courses");
 
+// Validating New Course Middleware
+const validateNewCourse = (req, res, next) => {
+  const { name } = req.body;
+  // Check Insufficient Data
+  if (!name) throw error(400, "Insufficient Data");
+  // Check Invalid Name (empty string)
+  if (name.trim().length === 0) throw error(400, "Invalid Name");
+  next();
+};
+
 router
   .route("/")
   // Get all courses
@@ -12,15 +22,13 @@ router
     res.json(courses);
   })
   // Create a new course
-  .post((req, res, next) => {
-    if (req.body.name) {
-      const course = {
-        id: courses.length ? courses[courses.length - 1].id + 1 : 1,
-        name: req.body.name,
-      };
-      courses.push(course);
-      res.status(201).json(courses);
-    } else throw error(400, "Insufficient Data");
+  .post(validateNewCourse, (req, res, next) => {
+    const course = {
+      id: courses.length ? courses[courses.length - 1].id + 1 : 1,
+      name: req.body.name,
+    };
+    courses.push(course);
+    res.status(201).json(courses);
   });
 
 module.exports = router;
