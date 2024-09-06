2ea157249a8d792a1922d601fb47f747e4c8e4ca
Dmytro Iovenko <iovenko@gmail.com>
2024-09-05 20:22:38 -0400

    add [POST /courses]

--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const error = require("../utils/error");
 
 // Get data
 const courses = require("../data/courses");
@@ -9,6 +10,17 @@ router
   // Get all courses
   .get((req, res, next) => {
     res.json(courses);
+  })
+  // Create a new course
+  .post((req, res, next) => {
+    if (req.body.name) {
+      const course = {
+        id: courses.length ? courses[courses.length - 1].id + 1 : 1,
+        name: req.body.name,
+      };
+      courses.push(course);
+      res.status(201).json(courses);
+    } else throw error(400, "Insufficient Data");
   });
 
 module.exports = router;
