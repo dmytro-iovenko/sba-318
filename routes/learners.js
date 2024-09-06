1d09170df32dcaf16cb1d1d0edb17ff756effb44
Dmytro Iovenko <iovenko@gmail.com>
2024-09-05 20:52:58 -0400

    add [POST /learners]

--- routes/learners.js
+++ routes/learners.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const error = require("../utils/error");
 
 // Get data
 const learners = require("../data/learners");
@@ -9,6 +10,24 @@ router
   // Get all learners
   .get((req, res, next) => {
     res.json(learners);
+  })
+  // Create a new learner
+  .post((req, res, next) => {
+    if (req.body.name && req.body.name.first_name && req.body.name.last_name && req.body.email) {
+      if (learners.find((l) => l.email.toLowerCase() === req.body.email.toLowerCase())) {
+        throw error(409, "Email Already Registered");
+      }
+      const learner = {
+        id: learners.length ? learners[learners.length - 1].id + 1 : 1,
+        name: {
+          first_name: req.body.name.first_name,
+          last_name: req.body.name.last_name,
+        },
+        email: req.body.email,
+      };
+      learners.push(learner);
+      res.status(201).json(learners);
+    } else throw error(400, "Insufficient Data");
   });
 
 module.exports = router;
